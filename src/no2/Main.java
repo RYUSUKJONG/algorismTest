8cdf5a6740f32ca9cfe853d0ef541ba6dc8914b1
Ryu <sookjong3367@naver.com>
2021-05-19 11:35:57 +0900

    알고리즘

--- src/no2/Main.java
+++ src/no2/Main.java
@@ -1,7 +1,34 @@
 package no2;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String[] str1 = {"leo", "kiki", "eden"};
+        String[] str2 = {"leo", "eden"};
+        System.out.println(solution(str1,str2));
+    }
+
+        static String solution(String[] participant, String[] completion) {
+            Map<String, Integer> cache = new HashMap<>();
+
+            for (int i = 0; i < participant.length; i++) {
+                    cache.put(participant[i], 1);
+            }
+
+            for (int i = 0; i < completion.length; i++) {
+                if (0 < cache.get(completion[i])) {
+                    cache.replace(completion[i], cache.get(completion[i]) - 1);
+                }
+            }
+            for (Map.Entry<String, Integer> item : cache.entrySet()) {
+                if (0 < item.getValue())
+                    return item.getKey();
+            }
+            return null;
 
     }
 };
